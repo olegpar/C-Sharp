4772acad4cebb082fc5dca12ae58ef199fdb4c58
Oleg Parfenyuk <olegpar@yandex.ru>
2022-12-14 21:08:42 +0300

    add magic2

--- Example007_magic/Program.cs
+++ Example007_magic/Program.cs
@@ -2,9 +2,9 @@
 //Console.SetCursorPosition(10, 4);
 //Console.WriteLine("+");
 
-int xa = 30, ya = 1,
-    xb = 1, yb = 20,
-    xc = 60, yc = 20;
+int xa = 20, ya = 1,
+    xb = 1, yb = 15,
+    xc = 40, yc = 15;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
